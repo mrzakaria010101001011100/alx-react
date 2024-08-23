bdc9d0b2e9968e63f15cc29d670011a1e3227741
zakaria derkaoui <141226786+mrzakaria010101001011100@users.noreply.github.com>
2024-08-23 22:01:56 +0200

    Update Notifications.js

--- 0x01-react_intro/task_1/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+
+import React from 'react';
+import './Notifications.css';
+
+export default function Notifications() {
+	return (
+		<div className="Notifications">
+			<p>
+				Here is the list of notifications
+			</p>
+		</div>
+	);
+};
