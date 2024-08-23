c6e90000162921e6cb2bf41737e477c39a474ebb
zakaria derkaoui <141226786+mrzakaria010101001011100@users.noreply.github.com>
2024-08-23 23:40:44 +0200

    Update header.js

--- 0x00-Webpack/task_3/modules/header/header.js
+++ 0x00-Webpack/task_3/modules/header/header.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const $ = require( "jquery" );
+import "./header.css";
+
+$('body').append("<div id='logo'></div>");
+$('body').append("<h1>Holberton Dashboard</h1>");
+
+console.log('Init header');
