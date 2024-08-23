b254d5101d594c19ff63e7862177cc8b1e8b89ed
zakaria derkaoui <141226786+mrzakaria010101001011100@users.noreply.github.com>
2024-08-23 23:33:20 +0200

    Update webpack.config.js

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: path.resolve(__dirname, './js/dashboard_main.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+};
