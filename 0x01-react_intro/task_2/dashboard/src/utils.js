5d2736f831fbc177f72f2d3b22a03cde26c886af
zakaria derkaoui <141226786+mrzakaria010101001011100@users.noreply.github.com>
2024-08-23 22:04:49 +0200

    Update utils.js

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -1,1 +1,22 @@
-#!/usr/bin/node
+const getFullYear = () => {
+	let current_year = new Date().getFullYear();
+	return current_year;
+}
+
+const getFooterCopy = (isIndex) => {
+	if (isIndex) {
+		return "Holberton School";
+	} else {
+		return "Holberton School main dashboard";
+	}
+}
+const getLatestNotification = () => {
+	return "<strong>Urgent requirement</strong> - complete by EOD";
+};
+
+
+module.exports = {
+	getLatestNotification,
+	getFooterCopy,
+	getFullYear,
+};
