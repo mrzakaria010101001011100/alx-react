ce1994fa48ec88fd0c8b76912305edec0a208086
zakaria derkaoui <141226786+mrzakaria010101001011100@users.noreply.github.com>
2024-08-30 23:30:15 +0200

    Update Login.test.js

--- 0x02-react_props/task_0/dashboard/src/Login/Login.test.js
+++ 0x02-react_props/task_0/dashboard/src/Login/Login.test.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+import React from 'react';
+import { shallow } from 'enzyme';
+import Login from './Login';
+
+const wrapper = shallow(<Login />);
+
+it('renders without crashing', () => {
+  shallow(<Login />);
+});
+it('renders login', () => {
+  expect(wrapper.find('main.login').exists()).toEqual(true);
+});
