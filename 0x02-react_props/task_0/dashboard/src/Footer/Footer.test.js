b9cf1b0ce60581a95fab9ed61590f8d27c33b7a5
zakaria derkaoui <141226786+mrzakaria010101001011100@users.noreply.github.com>
2024-08-30 23:27:49 +0200

    Update Footer.test.js

--- 0x02-react_props/task_0/dashboard/src/Footer/Footer.test.js
+++ 0x02-react_props/task_0/dashboard/src/Footer/Footer.test.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+import React from 'react';
+import { shallow } from 'enzyme';
+import Footer from './Footer';
+
+const wrapper = shallow(<Footer />);
+
+it('renders without crashing', () => {
+  shallow(<Footer />);
+});
+it('renders footer', () => {
+  expect(wrapper.find('footer.footer').exists()).toEqual(true);
+});
