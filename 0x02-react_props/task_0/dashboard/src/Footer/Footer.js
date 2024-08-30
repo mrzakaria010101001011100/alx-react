001ee08d7363f4736151866492dd6cf8404bddc6
zakaria derkaoui <141226786+mrzakaria010101001011100@users.noreply.github.com>
2024-08-30 23:27:21 +0200

    Update Footer.js

--- 0x02-react_props/task_0/dashboard/src/Footer/Footer.js
+++ 0x02-react_props/task_0/dashboard/src/Footer/Footer.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+import React from 'react';
+import './Footer.css';
+import { getFullYear, getFooterCopy } from '../utils/utils';
+
+function Footer() {
+  return (
+    <footer className='footer'>
+      <p>
+        Copyright {getFullYear()} - {getFooterCopy(true)}
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
