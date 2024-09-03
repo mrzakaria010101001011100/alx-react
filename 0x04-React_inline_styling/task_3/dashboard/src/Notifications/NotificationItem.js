acaf8bcf213ea57ab9824ba31f12730e97e013f2
zakaria derkaoui <141226786+mrzakaria010101001011100@users.noreply.github.com>
2024-09-03 22:18:05 +0200

    Update NotificationItem.js

--- 0x04-React_inline_styling/task_3/dashboard/src/Notifications/NotificationItem.js
+++ 0x04-React_inline_styling/task_3/dashboard/src/Notifications/NotificationItem.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+const NotificationItem = ({ type, value }) => {
+  return (
+    <li className={css(styles.item)}>
+      {value}
+    </li>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    width: '100%',
+    padding: '10px 8px',
+    borderBottom: '1px solid black',
+    fontSize: '20px',
+  },
+});
+
+export default NotificationItem;
