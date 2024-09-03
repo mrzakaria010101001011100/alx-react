abacee011e828a83de9065f6c29376f6d67dbf37
zakaria derkaoui <141226786+mrzakaria010101001011100@users.noreply.github.com>
2024-09-03 22:16:51 +0200

    Update Login.js

--- 0x04-React_inline_styling/task_3/dashboard/src/Login/Login.js
+++ 0x04-React_inline_styling/task_3/dashboard/src/Login/Login.js
@@ -1,1 +1,37 @@
 #!/usr/bin/node
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+const Login = () => {
+  return (
+    <div className={css(styles.loginContainer)}>
+      <label className={css(styles.label)}>Email:</label>
+      <input className={css(styles.input)} type="email" />
+      <label className={css(styles.label)}>Password:</label>
+      <input className={css(styles.input)} type="password" />
+      <button className={css(styles.button)}>Login</button>
+    </div>
+  );
+};
+
+const styles = StyleSheet.create({
+  loginContainer: {
+    '@media (max-width: 900px)': {
+      display: 'flex',
+      flexDirection: 'column',
+    },
+  },
+  label: {
+    display: 'block',
+    marginBottom: '5px',
+  },
+  input: {
+    display: 'block',
+    marginBottom: '10px',
+  },
+  button: {
+    marginTop: '10px',
+  },
+});
+
+export default Login;
