c1099cf15085611272ebaff30f0de4252f37d53d
zakaria derkaoui <141226786+mrzakaria010101001011100@users.noreply.github.com>
2024-09-03 15:53:46 +0200

    Update Header.js

--- 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.js
@@ -1,1 +1,27 @@
 #!/usr/bin/node
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+import logo from '../assets/holberton-logo.jpg';
+
+const styles = StyleSheet.create({
+  header: {
+    display: 'flex',
+    alignItems: 'center',
+    backgroundColor: '#282c34',
+    color: 'white',
+  },
+  logo: {
+    height: '200px',
+  },
+});
+
+function Header() {
+  return (
+    <header className={css(styles.header)}>
+      <img src={logo} className={css(styles.logo)} alt="Holberton Logo" />
+      <h1>School dashboard</h1>
+    </header>
+  );
+}
+
+export default Header;
