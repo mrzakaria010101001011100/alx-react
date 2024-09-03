a0f9c9000cd82b7af9d2c026e14e484c9d28d4a4
zakaria derkaoui <141226786+mrzakaria010101001011100@users.noreply.github.com>
2024-09-03 15:52:13 +0200

    Update CourseList.js

--- 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.js
@@ -1,1 +1,30 @@
 #!/usr/bin/node
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+import CourseListRow from './CourseListRow';
+
+const styles = StyleSheet.create({
+  table: {
+    width: '100%',
+    border: '1px solid #ddd',
+    marginTop: '20px',
+  },
+});
+
+function CourseList() {
+  return (
+    <table className={css(styles.table)}>
+      <thead>
+        <CourseListRow isHeader={true} textFirstCell="Available courses" />
+        <CourseListRow isHeader={true} textFirstCell="Course name" textSecondCell="Credit" />
+      </thead>
+      <tbody>
+        <CourseListRow textFirstCell="ES6" textSecondCell="60" />
+        <CourseListRow textFirstCell="Webpack" textSecondCell="20" />
+        <CourseListRow textFirstCell="React" textSecondCell="40" />
+      </tbody>
+    </table>
+  );
+}
+
+export default CourseList;
