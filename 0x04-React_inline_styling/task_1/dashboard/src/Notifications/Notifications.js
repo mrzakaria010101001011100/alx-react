ce148d1f1734d088f68406780540d43cb06f28d8
zakaria derkaoui <141226786+mrzakaria010101001011100@users.noreply.github.com>
2024-09-03 15:55:32 +0200

    Update Notifications.js

--- 0x04-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.js
@@ -1,1 +1,22 @@
 #!/usr/bin/node
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  notifications: {
+    border: '2px dashed red',
+    padding: '10px',
+    width: 'fit-content',
+    marginBottom: '20px',
+  },
+});
+
+function Notifications() {
+  return (
+    <div className={css(styles.notifications)}>
+      <p>Here is the list of notifications</p>
+    </div>
+  );
+}
+
+export default Notifications;
