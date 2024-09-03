e7b2f6f9b4ffdbda2c1dcdc49f5ffdb4cda33813
zakaria derkaoui <141226786+mrzakaria010101001011100@users.noreply.github.com>
2024-09-03 15:51:24 +0200

    Update BodySectionWithMarginBottom.js

--- 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+import BodySection from './BodySection';
+
+const styles = StyleSheet.create({
+  marginBottom: {
+    marginBottom: '40px',
+  },
+});
+
+function BodySectionWithMarginBottom(props) {
+  return (
+    <div className={css(styles.marginBottom)}>
+      <BodySection {...props} />
+    </div>
+  );
+}
+
+export default BodySectionWithMarginBottom;
