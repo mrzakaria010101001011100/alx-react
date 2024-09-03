d724b6ad05cc49f62096d4c8044aa0a982721a3a
zakaria derkaoui <141226786+mrzakaria010101001011100@users.noreply.github.com>
2024-09-03 15:55:00 +0200

    Update Login.js

--- 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Login/Login.js
@@ -1,1 +1,24 @@
 #!/usr/bin/node
+import React from 'react';
+import { StyleSheet, css } from 'aphrodite';
+
+const styles = StyleSheet.create({
+  login: {
+    margin: '40px',
+  },
+});
+
+function Login() {
+  return (
+    <div className={css(styles.login)}>
+      <h2>Login to access the full dashboard</h2>
+      <label htmlFor="email">Email:</label>
+      <input type="email" id="email" name="email" />
+      <label htmlFor="password">Password:</label>
+      <input type="password" id="password" name="password" />
+      <button type="button">OK</button>
+    </div>
+  );
+}
+
+export default Login;
