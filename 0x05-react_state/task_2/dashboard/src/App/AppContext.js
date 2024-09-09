262011a61b940e60f5c6767ffaa63ca0bf27112a
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-09-09 07:55:27 -0700

    React

--- 0x05-react_state/task_2/dashboard/src/App/AppContext.js
+++ 0x05-react_state/task_2/dashboard/src/App/AppContext.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+import React from "react";
+
+export const user = {
+  email: '',
+  password: '',
+  isLoggedIn: false
+};
+
+export function logOut() {
+  user.isLoggedIn = false;
+}
+
+export const AppContext = React.createContext({
+	user,
+	logOut
+});
