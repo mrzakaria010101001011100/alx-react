56e8d9993da70bc42880adc5505bb3c7bb8a2d65
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-09-25 03:59:07 -0700

    Reducer

--- 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
@@ -1,1 +1,38 @@
 #!/usr/bin/node
+// task_1/dashboard/src/reducers/uiReducer.js
+
+import { Map } from 'immutable';
+import { DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT } from '../actions/uiActionTypes';
+
+// Initial state using Immutable.js Map
+const initialState = Map({
+  isNotificationDrawerVisible: false,
+  isUserLoggedIn: false,
+  user: {}
+});
+
+// Reducer function
+const uiReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case DISPLAY_NOTIFICATION_DRAWER:
+      return state.set('isNotificationDrawerVisible', true);
+
+    case HIDE_NOTIFICATION_DRAWER:
+      return state.set('isNotificationDrawerVisible', false);
+
+    case LOGIN_SUCCESS:
+      return state.set('isUserLoggedIn', true);
+
+    case LOGIN_FAILURE:
+      return state.set('isUserLoggedIn', false);
+
+    case LOGOUT:
+      return state.set('isUserLoggedIn', false);
+
+    default:
+      return state;
+  }
+};
+
+export default uiReducer;
+
--- 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
@@ -1,1 +1,39 @@
 #!/usr/bin/node
+
+// task_1/dashboard/src/reducers/uiReducer.test.js
+
+import { Map } from 'immutable';
+import uiReducer from './uiReducer';
+import { DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from '../actions/uiActionTypes';
+
+describe('uiReducer', () => {
+  const initialState = Map({
+    isNotificationDrawerVisible: false,
+    isUserLoggedIn: false,
+    user: {}
+  });
+
+  it('should return the initial state when no action is passed', () => {
+    const state = uiReducer(undefined, {});
+    expect(state.toJS()).toEqual(initialState.toJS());
+  });
+
+  it('should return the initial state when the action SELECT_COURSE is passed', () => {
+    const action = { type: 'SELECT_COURSE' };
+    const state = uiReducer(undefined, action);
+    expect(state.toJS()).toEqual(initialState.toJS());
+  });
+
+  it('should change isNotificationDrawerVisible property when DISPLAY_NOTIFICATION_DRAWER action is passed', () => {
+    const action = { type: DISPLAY_NOTIFICATION_DRAWER };
+    const state = uiReducer(undefined, action);
+    expect(state.get('isNotificationDrawerVisible')).toBe(true);
+  });
+
+  it('should change isNotificationDrawerVisible property when HIDE_NOTIFICATION_DRAWER action is passed', () => {
+    const action = { type: HIDE_NOTIFICATION_DRAWER };
+    const state = uiReducer(undefined, action);
+    expect(state.get('isNotificationDrawerVisible')).toBe(false);
+  });
+});
+
