1573166f5a81a0eab6dc46dc1fa589f55ae94eb9
mrzakaria010101001011100 <zakariadrk00@gmail.com>
2024-10-02 13:51:35 -0700

    Reducer

--- 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/reducers/rootReducer.js
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/reducers/rootReducer.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+import { courseReducer } from "./courseReducer";
+import { notificationReducer } from "./notificationReducer";
+import { uiReducer } from "./uiReducers";
+import { combineReducers } from "redux";
+
+export const rootReducer = combineReducers({
+  courses: courseReducer,
+  notifications: notificationReducer,
+  ui: uiReducer
+})
